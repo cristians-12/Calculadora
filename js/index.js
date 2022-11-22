6b8482daf8539971c60036cb47af7b2c2cd5cd55
Cristian <97061228+Eggward12@users.noreply.github.com>
2022-11-22 14:50:46 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ $(".divi").click(function(){
     let numero_1 = parseInt($(".numero1").val());
     let numero_2 = parseInt($(".numero2").val());
     let resul = numero_1/numero_2;
-    $(".resul").html(resul);
+    $(".resul").html(resul.toFixed(2));
 })
 
 $(".exp").click(function(){
